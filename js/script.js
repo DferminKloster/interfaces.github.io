739fca488442972de1b66c380803a53edfb3f96b
Fermin Kloster <ferminkloster4@gmail.com>
2022-10-05 11:21:40 -0300

    agregue cambios y animaciones

---
+++ js/script.js
@@ -0,0 +1,11 @@
+document.querySelector(".barra_menu").addEventListener("click", animacionBarra);
+
+let line1__barra = document.querySelector(".line1__barra_menu");
+let line2__barra = document.querySelector(".line2__barra_menu");
+let line3__barra = document.querySelector(".line3__barra_menu");
+
+function animacionBarra(){
+    line1__barra.classList.toggle("activeline1__barra_menu");
+    line2__barra.classList.toggle("activeline2__barra_menu");
+    line3__barra.classList.toggle("activeline3__barra_menu");
+}
