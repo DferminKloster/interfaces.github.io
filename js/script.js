a6c422b56fc134e4602ec671d60bd2e47c8392d7
Fermin Kloster <ferminkloster4@gmail.com>
2022-10-09 17:49:49 -0300

    arreglue las card

--- js/script.js
+++ js/script.js
@@ -3,9 +3,15 @@ document.querySelector(".barra_menu").addEventListener("click", animacionBarra);
 let line1__barra = document.querySelector(".line1__barra_menu");
 let line2__barra = document.querySelector(".line2__barra_menu");
 let line3__barra = document.querySelector(".line3__barra_menu");
+let container__menu = document.querySelector(".menu");
 
 function animacionBarra(){
     line1__barra.classList.toggle("activeline1__barra_menu");
     line2__barra.classList.toggle("activeline2__barra_menu");
     line3__barra.classList.toggle("activeline3__barra_menu");
+
+    container__menu.classList.toggle("activemenu");
 }
+
+
+
