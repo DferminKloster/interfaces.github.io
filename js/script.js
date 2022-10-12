ddc44fa03676e5647b2782ea4619ea7e2bfa7551
Fermin Kloster <ferminkloster4@gmail.com>
2022-10-12 01:57:47 -0300

    agregue el menu del menu de hamburguesas

--- js/script.js
+++ js/script.js
@@ -3,15 +3,19 @@ document.querySelector(".barra_menu").addEventListener("click", animacionBarra);
 let line1__barra = document.querySelector(".line1__barra_menu");
 let line2__barra = document.querySelector(".line2__barra_menu");
 let line3__barra = document.querySelector(".line3__barra_menu");
-let container__menu = document.querySelector(".menu");
+let desplegable = document.querySelector(".menu-navbar");
+let btnMenu = document.querySelector(".barra_menu");
 
 function animacionBarra(){
     line1__barra.classList.toggle("activeline1__barra_menu");
     line2__barra.classList.toggle("activeline2__barra_menu");
     line3__barra.classList.toggle("activeline3__barra_menu");
 
-    container__menu.classList.toggle("activemenu");
 }
 
 
 
+btnMenu.addEventListener("click", () => {
+    desplegable.classList.toggle("menu-navbar-visible");
+    
+})
