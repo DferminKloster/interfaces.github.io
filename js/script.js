92afb91a1e41de7a27c1f1d115e21fe88d86f828
Fermin Kloster <ferminkloster4@gmail.com>
2022-10-12 11:20:11 -0300

    agregue el menu de carga de la pagina

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+"use strict";
+
 document.querySelector(".barra_menu").addEventListener("click", animacionBarra);
 
 let line1__barra = document.querySelector(".line1__barra_menu");
@@ -6,6 +8,8 @@ let line3__barra = document.querySelector(".line3__barra_menu");
 let desplegable = document.querySelector(".menu-navbar");
 let btnMenu = document.querySelector(".barra_menu");
 
+
+/*animacion del menu de hamburguesas*/ 
 function animacionBarra(){
     line1__barra.classList.toggle("activeline1__barra_menu");
     line2__barra.classList.toggle("activeline2__barra_menu");
@@ -13,9 +17,30 @@ function animacionBarra(){
 
 }
 
-
-
+/*menu de hamburguesas*/ 
 btnMenu.addEventListener("click", () => {
     desplegable.classList.toggle("menu-navbar-visible");
     
 })
+
+/*carga al inicio de empezar*/ 
+
+function calcularPorcentajeCarga() {
+    let porcentaje = 0;
+    const textPorcentaje = document.querySelector("#porcentaje-carga");
+    if (textPorcentaje) {
+        const contenedorCarga = document.querySelector("#contenedor-carga");
+        const intervalo = setInterval(() => {
+            if (porcentaje < 100) {
+                porcentaje = porcentaje + 10;
+                textPorcentaje.innerHTML = `${porcentaje}%`;
+            }
+        }, 500);
+        setTimeout(() => {
+            clearInterval(intervalo);
+            contenedorCarga.classList.toggle("contenedor-carga-hidden");
+        }, 5000);
+    }
+}
+
+calcularPorcentajeCarga();
