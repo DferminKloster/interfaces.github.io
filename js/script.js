58214ff1fde2ad258387f06e693d2475a9f74e36
jcampo <juanmanuelc563@gmail.com>
2022-10-25 17:05:14 -0300

    barra lateral finalizada

--- js/script.js
+++ js/script.js
@@ -1,14 +1,12 @@
 "use strict";
-
-document.querySelector(".barra_menu").addEventListener("click", animacionBarra);
-document.getElementById("game_btn").addEventListener("click", redireccionar);;
-document.getElementById("button2").addEventListener("click", redireccionar2);
-
 let line1__barra = document.querySelector(".line1__barra_menu");
 let line2__barra = document.querySelector(".line2__barra_menu");
 let line3__barra = document.querySelector(".line3__barra_menu");
 let desplegable = document.querySelector(".menu-navbar");
 let btnMenu = document.querySelector(".barra_menu");
+
+document.querySelector(".barra_menu").addEventListener("click", animacionBarra);
+
 
 
 /*animacion del menu de hamburguesas*/ 
@@ -17,13 +15,6 @@ function animacionBarra(){
     line2__barra.classList.toggle("activeline2__barra_menu");
     line3__barra.classList.toggle("activeline3__barra_menu");
 
-}
-
-function redireccionar() {
-    window.location.href = "juego.html";
-}
-function redireccionar2() {
-    window.location.href = "proximojuego.html";
 }
 
 /*menu de hamburguesas*/ 
