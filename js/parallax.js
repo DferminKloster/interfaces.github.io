f48ed8f5e0e194c29eca8fd95a9813b9690b2e4a
Fermin Kloster <ferminkloster4@gmail.com>
2022-11-27 15:24:46 -0300

    haciando la parte del nuevo lanzamiento

--- js/parallax.js
+++ js/parallax.js
@@ -1,45 +1,66 @@
 let bg = document.querySelector(".BG");
 let title = document.querySelector(".CS-title");
-let mercy = document.querySelector(".mercy");
-let winston = document.querySelector(".winston");
-let janzo = document.querySelector(".janzo");
 let texto = document.querySelector(".texto-juego-coming-soon")
-let img = document.querySelector(".imagenes");
-let descript = document.querySelector(".descript");
-let info = document.querySelector(".info");
-let carrousel = document.querySelector(".carrousel-container");
-let featuring = document.querySelector(".carrousel");
-let hero = document.querySelector(".personajes");
+const mercy = document.querySelector(".mercy");
+const winston = document.querySelector(".winston");
+const janzo = document.querySelector(".janzo");
+const img = document.querySelector(".imagenes");
+const descript = document.querySelector(".descript");
+const info = document.querySelector(".info");
+const carrousel = document.querySelector(".carrousel-container");
+const featuring = document.querySelector(".carrousel");
+const hero = document.querySelector(".personajes");
 
 window.addEventListener("scroll", () =>{
     var value = window.scrollY;
 
-    img.style.animationName = "none";
     winston.style.animationName = "none";
     mercy.style.animationName = "none";
     janzo.style.animationName = "none";
-    descript.style.animationName = "none";
-    info.style.animationName = "none";
-    carrousel.style.animationName = "none";
-    featuring.style.animationName = "none";
     hero.style.animationName = "none";
 
-    bg.style.left = -value * 0.5 + 'px';
-    title.style.top = -value * 0.5  + 'px';
+    title.style.top = value * 0.5  + 'px';
     texto.style.left= -value * 0.5 + 'px';
 
     setTimeout(() => {
-        img.style.animationName = "imgoverwach";
         winston.style.animationName = "winston";
         mercy.style.animationName = "mercy";
         janzo.style.animationName = "janzo";
-        descript.style.animationName = "descript";
-        info.style.animationName = "info";
-        carrousel.style.animationName = "carrousel";
-        featuring.style.animationName = "featuring";
-        hero.style.animationName = "hero";
+        hero.style.animationName = "hero"; 
     }, 0);
 });
+
+
+const cargarImagen = (entradas, observador) => {
+    console.log(entradas)
+    console.log(observador)
+
+	entradas.forEach((entrada) => {
+		if(entrada.isIntersecting){
+			entrada.target.classList.add('visible');
+		} else {
+			// entrada.target.classList.remove('visible');
+		}
+	});
+}
+
+const observador = new IntersectionObserver(cargarImagen, {
+	root: null,
+	rootMargin: '0px 0px 0px 0px',
+	threshold: 0.5
+});
+
+observador.observe(img);
+observador.observe(descript);
+observador.observe(info);
+observador.observe(carrousel);
+observador.observe(featuring);
+
+
+
+
+
+
 
 
 
